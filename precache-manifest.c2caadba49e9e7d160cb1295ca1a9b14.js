1f2ae68a59d869ca4de0c3790777e69892b698c1
Alexis Métaireau <alexis@notmyidea.org>
2023-01-16 00:54:11 +0100

    Update documentation

---
+++ precache-manifest.c2caadba49e9e7d160cb1295ca1a9b14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf8de95d17fd044052cc2cf28dab632",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfd30ec0b1f2359b99063c66e7636933",
+    "url": "/logo.webp"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "af5facbbfb3fd4607c02",
+    "url": "/static/css/main.5c4ed42b.chunk.css"
+  },
+  {
+    "revision": "2bc52b0b136a46e117e5",
+    "url": "/static/css/vendors~main.149c4fd3.chunk.css"
+  },
+  {
+    "revision": "af5facbbfb3fd4607c02",
+    "url": "/static/js/main.af5facbb.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "2bc52b0b136a46e117e5",
+    "url": "/static/js/vendors~main.2bc52b0b.chunk.js"
+  }
+]);
