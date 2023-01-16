845ad7558c6586fd7207c0a459c8549477091897
Alexis Métaireau <alexis@notmyidea.org>
2023-01-16 01:23:21 +0100

    Update documentation

---
+++ precache-manifest.b49930a059528c4e0c0e9593d8125360.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a766744287e01f1f3b41ee3172f85db",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "5f0f58e4da841da5735e0deca629201c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfd30ec0b1f2359b99063c66e7636933",
+    "url": "/logo.webp"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "c69e60898368e7c3b27b",
+    "url": "/static/css/main.5c4ed42b.chunk.css"
+  },
+  {
+    "revision": "2bc52b0b136a46e117e5",
+    "url": "/static/css/vendors~main.149c4fd3.chunk.css"
+  },
+  {
+    "revision": "c69e60898368e7c3b27b",
+    "url": "/static/js/main.c69e6089.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "2bc52b0b136a46e117e5",
+    "url": "/static/js/vendors~main.2bc52b0b.chunk.js"
+  }
+]);
